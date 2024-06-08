bde6b93b294c1a752c4048b910094700c8347df9
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-06-08 19:30:39 +0300

    Add html in App #7

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -1,4 +1,44 @@
-<template>VUE APP</template>
+<template>
+    <main>
+        <div>
+            <h3>Задачник</h3>
+        </div>
+        <div>
+            <div>
+                <form @submit.prevent="addTodo">
+                    <h3>Напишите свою задачу</h3>
+                    <input 
+                        type="text"
+                        placeholder="Время объявить свои планы!"
+                        v-model="text">
+                    <input type="submit" value="Добавить задачу"/>
+                </form>
+            </div>
+        </div>
+        <div>
+            <div>
+                <h2 v-show="todos.length === 0">Нет задач</h2>
+                <div>
+                    <div 
+                        v-for="todo in todos"
+                        :class="`todo-item ${todo.done && 'done'}`"
+                        :key="todo.id"
+                    >
+                        <label>
+                            <input type="checkbox" v-model="todo.done" />
+                        </label>
+                        <div>
+                            <input type="text" v-model="todo.value" />
+                        </div>
+                        <div>
+                            <button @click="deleteTodo(todo)">Удалить</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
 
 <script setup>
     import { ref, onMounted, watch } from 'vue';
@@ -10,6 +50,7 @@
         if (text.value.trim() === "") return;
 
         todos.value.unshift({
+            id: Date.now(),
             value: text.value,
             done: false,
         })
@@ -24,7 +65,7 @@
     watch(
         todos,
         (newTodo) => {
-            localStorage.setItem("todos", JSON.stringify(newTodo))
+            localStorage.setItem("todos", JSON.stringify(newTodo));
         },
         { deep: true }
     );
