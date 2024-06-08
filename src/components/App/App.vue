7adc2bad328125daf830ddf529c8e3246e2d66cc
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-06-08 18:30:30 +0300

    Add addTodo function in App #3

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -1,5 +1,19 @@
 <template>VUE APP</template>
 
 <script setup>
+    import { ref, onMounted, watch } from 'vue';
 
+    const todos = ref([]);
+    const text = ref("");
+
+    const addTodo = () => {
+        if (text.value.trim() === "") return;
+
+        todos.value.unshift({
+            value: text.value,
+            done: false,
+        })
+
+        text.value = "";
+    }
 </script>
